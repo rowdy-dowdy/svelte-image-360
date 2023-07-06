d795ba3a332d34377ef752c712e7cfadc643e138
Viet Hung <viet.hung.2898@gmail.com>
2023-07-06 14:04:52 +0700

    add

--- src/routes/admin/(admin)/+page.svelte
+++ src/routes/admin/(admin)/+page.svelte
@@ -61,15 +61,17 @@
 <div class="w-full h-full flex items-stretch">
   <div class="w-80 flex-none bg-white py-4 border-r flex flex-col space-y-6">
     <div class="hidden relative md:block px-4">
-      <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+      <div class="flex absolute inset-y-0 left-0 items-center pl-5 pointer-events-none">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 dark:text-white"><path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" /></svg>
       </div>
       <Input id="search-navbar" class="pl-10" placeholder="Tìm kiếm..." bind:value={search} />
       {#if search != ""}
-        <div class="flex absolute inset-y-0 right-0 items-center pr-3 cursor-pointer"
+        <div class="flex absolute inset-y-1 right-5 items-center px-2 cursor-pointer rounded bg-primary-600 hover:bg-primary-500 text-white"
           on:click={() => search = ""}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
+          <span class="material-symbols-outlined">
+            search
+          </span>
         </div>
       {/if}
     </div>
